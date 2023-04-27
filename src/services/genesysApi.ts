96effe3a1bd0eeb6b276afb346b2f332a8615615
Yuliia Dikun <dikun.yulia@gmail.com>
2023-04-27 22:33:06 +0300

    added: genesysAPI class

---
+++ src/services/genesysApi.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosResponse } from "axios";
+import { toast } from "react-toastify";
+
+class WiseyAPI {
+  private baseURL: string;
+
+  constructor() {
+    this.baseURL = "https://api.wisey.app/api/v1";
+  }
+
+  setToken(token: string): void {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  }
+
+  async getToken(): Promise<void> {
+    try {
+      const res: AxiosResponse<{ token: string }> = await axios.get(
+        `${this.baseURL}/auth/anonymous?platform=subscriptions`
+      );
+      this.setToken(res.data.token);
+    } catch (error) {
+      toast.error("Something goes wrong.");
+    }
+  }
+
+  async getCourses(): Promise<any> {
+    try {
+      await this.getToken();
+      const res: AxiosResponse<any> = await axios.get(`${this.baseURL}/core/preview-courses`);
+      return res.data;
+    } catch (error) {
+      toast.error("Something goes wrong.");
+    }
+  }
+
+  async getCourseById(id: number): Promise<any> {
+    try {
+      await this.getToken();
+      const res: AxiosResponse<any> = await axios.get(`${this.baseURL}/core/preview-courses/${id}`);
+      return res.data;
+    } catch (error) {
+      toast.error("Something goes wrong.");
+    }
+  }
+}
+
+const wiseyAPI = new WiseyAPI();
+export default wiseyAPI;
